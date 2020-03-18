4505e78a945bbfe3ecbd296747e9fb60c6c1be89
Hua-Shan Wu <greenballow@gmail.com>
2020-03-18 16:23:01 +0800

    M

--- main.py
+++ main.py
@@ -43,49 +43,40 @@ if '-99.000' in C0A880_HUMD or '-999.000' in C0A880_HUMD:
 else:
    # Sum
    C0A880_Sum = sum(list(map(float,C0A880_HUMD)))
-
+#===
 if '-99.000' in C0F9A0_HUMD or '-999.000' in C0F9A0_HUMD:
    C0F9A0_HUMD['-99.000'] = "None"
    C0F9A0_HUMD['-999.000'] = "None"
    C0F9A0_Sum = "None"
 else:
    C0F9A0_Sum = sum(list(map(float,C0F9A0_HUMD)))
-
+#===
 if '-99.000' in C0G640_HUMD or '-999.000' in C0G640_HUMD:
    C0G640_HUMD['-99.000'] = "None"
    C0G640_HUMD['-999.000'] = "None"
    C0G640_Sum = "None"
 else:
    C0G640_Sum = sum(list(map(float,C0G640_HUMD)))
-
+#===
 if '-99.000' in C0R190_HUMD or '-999.000' in C0R190_HUMD:
    C0R190_HUMD['-99.000'] = "None"
    C0R190_HUMD['-999.000'] = "None"
    C0R190_Sum = "None"
 else:
    C0R190_Sum = sum(list(map(float,C0R190_HUMD)))
-
+#===
 if '-99.000' in C0X260_HUMD or '-999.000' in C0X260_HUMD:
    C0X260_HUMD['-99.000'] = "None"
    C0X260_HUMD['-999.000'] = "None"
    C0X260_Sum = "None"
 else:
    C0X260_Sum = sum(list(map(float,C0X260_HUMD)))
-
-
 #=======================================
-
-
 # Part. 4
-
 #=======================================
-
 # Print result
-
 #print(C0A880_HUMD)
 #print(C0A880_Sum)
 #print(['C0A880', C0A880_Sum])
 print([['C0A880', C0A880_Sum], ['C0F9A0', C0F9A0_Sum], ['C0G640', C0G640_Sum], ['C0R190', C0R190_Sum], ['C0X260', C0X260_Sum]])
-
-
 #========================================
